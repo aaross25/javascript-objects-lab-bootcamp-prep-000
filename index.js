838a245553d29ecefe909df3dfe7001fcd20ff26
Ariel Ross <arielross92@gmail.com>
2017-12-29 02:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function deleteFromObjectByKey(object,key)
 function destructivelyDeleteFromObjectByKey(object,key)
 {
   var object = { key: 'value' }
-  delete object.key
-  return object
+  return delete object.key
+
 }
