8ae17199e6b9030616072a4b60f1b384ca66ce2b
Ariel Ross <arielross92@gmail.com>
2017-12-29 01:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value)
 {
-  object[key] = value
-  return object
+  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value)
 {
-  return
+  object[key] = value
+  return object
 }
 
 function deleteFromObjectByKey(object,key)
